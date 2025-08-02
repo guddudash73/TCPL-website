f6aa48acbae9f95fb9722ce4e7f721290f7e3627
Rashmiranjan_Dash <guddudash73@gmail.com>
2025-08-03 02:40:16 +0530

    added dropdown in navbar & impovements

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import tcpl_logo from "../../assets/tcpl_logo.png";
-
+import { motion, AnimatePresence } from "motion/react";
 const Navbar = () => {
+  const [isServiceOpen, setIsServiceOpen] = useState(false);
+
+  const handleServicesOpen = () => {
+    setIsServiceOpen(!isServiceOpen);
+  };
+
   const navLinks = (
-    <ul className=" font-primary font-medium text-sm flex space-x-8">
-      <li>
+    <div className=" font-primary font-medium text-sm flex md:space-x-8 space-x-4">
+      <div>
         <a href="/">Home</a>
-      </li>
-      <li>
-        <a href="/work">Work</a>
-      </li>
-      <li>
+      </div>
+      <div
+        className="relative flex-row md:flex-col"
+        onMouseEnter={handleServicesOpen}
+        onMouseLeave={() => {
+          setTimeout(() => {
+            handleServicesOpen();
+          }, 180);
+        }}
+      >
+        <label>Work</label>
+        <AnimatePresence>
+          {isServiceOpen && (
+            <div className="absolute w-60 h-auto">
+              <motion.div
+                className="bg-accent/25 p-2 flex flex-col justify-evenly rounded-md mt-3"
+                initial={{ opacity: 0, height: 0, minHeight: 100 }}
+                animate={{ opacity: 1, height: 150 }}
+                exit={{ opacity: 0, height: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                <div>GIS Services</div>
+                <div>LiDAR Services</div>
+                <div>Software Deleopment Services</div>
+              </motion.div>
+            </div>
+          )}
+        </AnimatePresence>
+      </div>
+      <div>
         <a href="/customer">Our Customer</a>
-      </li>
-      <li>
+      </div>
+      <div>
         <a href="/team">Team</a>
-      </li>
-      <li>
+      </div>
+      <div>
         <a href="/career">Career</a>
-      </li>
-      <li>
+      </div>
+      <div>
         <a href="/contact">Contact</a>
-      </li>
-    </ul>
+      </div>
+    </div>
   );
   return (
     <div className="w-full h-16 shadow fixed top-0 left-0">
